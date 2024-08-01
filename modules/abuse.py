2eaf95de109194014346f77e2b46eb808daaa164
gorkrog <107999943+gorkrog@users.noreply.github.com>
2024-08-01 13:18:19 +0200

    small update
    added response code check

--- modules/abuse.py
+++ modules/abuse.py
@@ -9,7 +9,7 @@ def abuse_add_ip_header(api_check: bool) -> str:
         return f''
 
 
-def abuse_check_ip(ip_address: str, abuse_aip: str) -> list:
+def abuse_check_ip(ip_address: str, abuse_api: str) -> list:
     url = 'https://api.abuseipdb.com/api/v2/check'
     querystring = {
         'ipAddress': f'{ip_address}',
@@ -17,21 +17,25 @@ def abuse_check_ip(ip_address: str, abuse_aip: str) -> list:
     }
     headers = {
         'Accept': 'application/json',
-        'Key': f'{abuse_aip}'
+        'Key': f'{abuse_api}'
     }
     response = requests.request(method='GET', url=url, headers=headers, params=querystring)
-    decoded_response = json.loads(response.text)
-    abuse_data = [
-        decoded_response["data"]["ipAddress"],
-        decoded_response["data"]["isPublic"],
-        decoded_response["data"]["isWhitelisted"],
-        decoded_response["data"]["abuseConfidenceScore"],
-        decoded_response["data"]["countryCode"],
-        decoded_response["data"]["usageType"],
-        decoded_response["data"]["isp"],
-        decoded_response["data"]["domain"],
-        decoded_response["data"]["hostnames"],
-        decoded_response["data"]["isTor"],
-        decoded_response["data"]["totalReports"]
-    ]
-    return abuse_data
+    if response.status_code == 200:
+        decoded_response = json.loads(response.text)
+        abuse_data = [
+            decoded_response["data"]["ipAddress"],
+            decoded_response["data"]["isPublic"],
+            decoded_response["data"]["isWhitelisted"],
+            decoded_response["data"]["abuseConfidenceScore"],
+            decoded_response["data"]["countryCode"],
+            decoded_response["data"]["usageType"],
+            decoded_response["data"]["isp"],
+            decoded_response["data"]["domain"],
+            decoded_response["data"]["hostnames"],
+            decoded_response["data"]["isTor"],
+            decoded_response["data"]["totalReports"]
+        ]
+        return abuse_data
+    else:
+        abuse_data = ['response status code', response.status_code]
+        return abuse_data
--- modules/vt.py
+++ modules/vt.py
@@ -17,32 +17,35 @@ def vt_check_ip(ip_address: str, vt_aip: str) -> list:
         "x-apikey": f"{vt_aip}"
     }
     response = requests.request(method='GET', url=url, headers=headers)
-    decoded_response = json.loads(response.text)
-    vt_data = [
-        ip_address,
-        decoded_response["data"]["attributes"]["last_https_certificate_date"],
-        decoded_response["data"]["attributes"]["network"],
-        decoded_response["data"]["attributes"]["last_modification_date"],
-        decoded_response["data"]["attributes"]["as_owner"],
-        decoded_response["data"]["attributes"]["asn"],
-        decoded_response["data"]["attributes"]["last_analysis_stats"]["malicious"],
-        decoded_response["data"]["attributes"]["last_analysis_stats"]["suspicious"],
-        decoded_response["data"]["attributes"]["last_analysis_stats"]["undetected"],
-        decoded_response["data"]["attributes"]["last_analysis_stats"]["harmless"],
-        decoded_response["data"]["attributes"]["last_analysis_stats"]["timeout"],
-        decoded_response["data"]["attributes"]["total_votes"]["harmless"],
-        decoded_response["data"]["attributes"]["total_votes"]["malicious"],
-        decoded_response["data"]["attributes"]["reputation"],
-        decoded_response["data"]["attributes"]["last_analysis_date"],
-        decoded_response["data"]["links"]["self"]
-    ]
-    for i, item in enumerate(vt_data):
-        if item == int:
-            if item > 100000000:
-                vt_data[i] = datetime.datetime.fromtimestamp(item)
+    if response.status_code == 200:
+        decoded_response = json.loads(response.text)
+        vt_data = [
+            ip_address,
+            decoded_response["data"]["attributes"]["last_https_certificate_date"],
+            decoded_response["data"]["attributes"]["network"],
+            decoded_response["data"]["attributes"]["last_modification_date"],
+            decoded_response["data"]["attributes"]["as_owner"],
+            decoded_response["data"]["attributes"]["asn"],
+            decoded_response["data"]["attributes"]["last_analysis_stats"]["malicious"],
+            decoded_response["data"]["attributes"]["last_analysis_stats"]["suspicious"],
+            decoded_response["data"]["attributes"]["last_analysis_stats"]["undetected"],
+            decoded_response["data"]["attributes"]["last_analysis_stats"]["harmless"],
+            decoded_response["data"]["attributes"]["last_analysis_stats"]["timeout"],
+            decoded_response["data"]["attributes"]["total_votes"]["harmless"],
+            decoded_response["data"]["attributes"]["total_votes"]["malicious"],
+            decoded_response["data"]["attributes"]["reputation"],
+            decoded_response["data"]["attributes"]["last_analysis_date"],
+            decoded_response["data"]["links"]["self"]
+        ]
+        for i, item in enumerate(vt_data):
+            if item == int:
+                if item > 100000000:
+                    vt_data[i] = datetime.datetime.fromtimestamp(item)
 
-    return vt_data
-
+        return vt_data
+    else:
+        vt_data = ['response status code', response.status_code]
+        return vt_data
 
 def vt_add_domain_header(api_check: bool) -> str:
     if api_check:
@@ -58,33 +61,36 @@ def vt_check_domain(domain: str, vt_aip: str) -> list:
         "x-apikey": f"{vt_aip}"
     }
     response = requests.request(method='GET', url=url, headers=headers)
-    decoded_response = json.loads(response.text)
-    vt_data = [
-        domain,
-        decoded_response["data"]["id"],
-        decoded_response["data"]["attributes"]["last_dns_records_date"],
-        decoded_response["data"]["attributes"]["last_modification_date"],
-        decoded_response["data"]["attributes"]["reputation"],
-        # decoded_response["data"]["attributes"]["whois"],
-        decoded_response["data"]["attributes"]["whois_date"],
-        decoded_response["data"]["attributes"]["last_https_certificate_date"],
-        decoded_response["data"]["attributes"]["last_analysis_stats"]["malicious"],
-        decoded_response["data"]["attributes"]["last_analysis_stats"]["suspicious"],
-        decoded_response["data"]["attributes"]["last_analysis_stats"]["undetected"],
-        decoded_response["data"]["attributes"]["last_analysis_stats"]["harmless"],
-        decoded_response["data"]["attributes"]["last_analysis_stats"]['timeout'],
-        decoded_response["data"]["attributes"]["total_votes"]["harmless"],
-        decoded_response["data"]["attributes"]["total_votes"]["malicious"],
-        decoded_response["data"]["attributes"]["reputation"],
-        decoded_response["data"]["attributes"]["last_analysis_date"],
-        decoded_response["data"]["links"]["self"]
-    ]
-    for i, item in enumerate(vt_data):
-        if item == int:
-            if item > 100000000:
-                vt_data[i] = datetime.datetime.fromtimestamp(item)
+    if response.status_code == 200:
+        decoded_response = json.loads(response.text)
+        vt_data = [
+            domain,
+            decoded_response["data"]["id"],
+            decoded_response["data"]["attributes"]["last_dns_records_date"],
+            decoded_response["data"]["attributes"]["last_modification_date"],
+            decoded_response["data"]["attributes"]["reputation"],
+            decoded_response["data"]["attributes"]["whois_date"],
+            decoded_response["data"]["attributes"]["last_https_certificate_date"],
+            decoded_response["data"]["attributes"]["last_analysis_stats"]["malicious"],
+            decoded_response["data"]["attributes"]["last_analysis_stats"]["suspicious"],
+            decoded_response["data"]["attributes"]["last_analysis_stats"]["undetected"],
+            decoded_response["data"]["attributes"]["last_analysis_stats"]["harmless"],
+            decoded_response["data"]["attributes"]["last_analysis_stats"]['timeout'],
+            decoded_response["data"]["attributes"]["total_votes"]["harmless"],
+            decoded_response["data"]["attributes"]["total_votes"]["malicious"],
+            decoded_response["data"]["attributes"]["reputation"],
+            decoded_response["data"]["attributes"]["last_analysis_date"],
+            decoded_response["data"]["links"]["self"]
+        ]
+        for i, item in enumerate(vt_data):
+            if item == int:
+                if item > 100000000:
+                    vt_data[i] = datetime.datetime.fromtimestamp(item)
 
-    return vt_data
+        return vt_data
+    else:
+        vt_data = ['response status code', response.status_code]
+        return vt_data
 
 
 def vt_add_hash_header(api_check: bool) -> str:
@@ -101,29 +107,33 @@ def vt_check_hash(file_hash: str, vt_aip: str) -> list:
         "x-apikey": f"{vt_aip}"
     }
     response = requests.request(method='GET', url=url, headers=headers)
-    decoded_response = json.loads(response.text)
-    vt_data = [
-        file_hash,
-        decoded_response["data"]["attributes"]["signature_info"]["original name"],
-        decoded_response["data"]["attributes"]["signature_info"]["description"],
-        decoded_response["data"]["attributes"]["md5"],
-        decoded_response["data"]["attributes"]["sha1"],
-        decoded_response["data"]["attributes"]["sha256"],
-        decoded_response["data"]["attributes"]["first_submission_date"],
-        decoded_response["data"]["attributes"]["last_submission_date"],
-        decoded_response["data"]["attributes"]["last_analysis_date"],
-        decoded_response["data"]["attributes"]["last_analysis_stats"]["malicious"],
-        decoded_response["data"]["attributes"]["last_analysis_stats"]["suspicious"],
-        decoded_response["data"]["attributes"]["last_analysis_stats"]["undetected"],
-        decoded_response["data"]["attributes"]["last_analysis_stats"]["harmless"],
-        decoded_response["data"]["attributes"]["last_analysis_stats"]["timeout"],
-        decoded_response["data"]["attributes"]["last_analysis_stats"]["failure"],
-        decoded_response["data"]["attributes"]["last_analysis_stats"]["type-unsupported"],
-        decoded_response["data"]["links"]["self"]
-    ]
-    for i, item in enumerate(vt_data):
-        if item == int:
-            if item > 100000000:
-                vt_data[i] = datetime.datetime.fromtimestamp(item)
+    if response.status_code == 200:
+        decoded_response = json.loads(response.text)
+        vt_data = [
+            file_hash,
+            decoded_response["data"]["attributes"]["signature_info"]["original name"],
+            decoded_response["data"]["attributes"]["signature_info"]["description"],
+            decoded_response["data"]["attributes"]["md5"],
+            decoded_response["data"]["attributes"]["sha1"],
+            decoded_response["data"]["attributes"]["sha256"],
+            decoded_response["data"]["attributes"]["first_submission_date"],
+            decoded_response["data"]["attributes"]["last_submission_date"],
+            decoded_response["data"]["attributes"]["last_analysis_date"],
+            decoded_response["data"]["attributes"]["last_analysis_stats"]["malicious"],
+            decoded_response["data"]["attributes"]["last_analysis_stats"]["suspicious"],
+            decoded_response["data"]["attributes"]["last_analysis_stats"]["undetected"],
+            decoded_response["data"]["attributes"]["last_analysis_stats"]["harmless"],
+            decoded_response["data"]["attributes"]["last_analysis_stats"]["timeout"],
+            decoded_response["data"]["attributes"]["last_analysis_stats"]["failure"],
+            decoded_response["data"]["attributes"]["last_analysis_stats"]["type-unsupported"],
+            decoded_response["data"]["links"]["self"]
+        ]
+        for i, item in enumerate(vt_data):
+            if item == int:
+                if item > 100000000:
+                    vt_data[i] = datetime.datetime.fromtimestamp(item)
 
-    return vt_data
+        return vt_data
+    else:
+        vt_data = ['response status code', response.status_code]
+        return vt_data
